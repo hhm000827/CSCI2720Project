ffbe26e73bfb1d2ff7e7a65581592aee8401df62
hhm000827 <hauhoman@yahoo.com.hk>
2022-11-23 23:05:02 +0800

    create CR operation for comment

---
+++ server/server.mjs
@@ -0,0 +1,29 @@
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { createComment, findCommentsByLocation } from "./mongodb/commentTable/commentCrud.mjs";
+
+const require = createRequire(import.meta.url);
+const express = require("express");
+const path = require("path");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+
+app.get("/api/createComment", (req, res) => {
+  const commentObject = {
+    location: req.query["location"],
+    username: req.query["username"],
+    comment: req.query["comment"],
+  };
+  createComment(commentObject);
+  res.send("Success");
+});
+
+app.get("/api/findCommentsByLocation", (req, res) => {
+  const location = req.query["location"];
+  findCommentsByLocation(location);
+  res.send("Success");
+});
+
+app.use(express.static(path.join(__dirname, "../", "build")));
+const server = app.listen(80);
