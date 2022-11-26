96ad4e6050e907fc54a9dbfb455b0ccc443634b6
Karrix <karrixlee1231@gmail.com>
2022-11-26 22:31:03 +0800

    Add: error toast

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,47 +1,56 @@
+import toast, { Toaster } from "react-hot-toast";
+
 export function Login() {
     return (
-        <div className="flex w-full h-screen items-center justify-center">
-            <div className="card max-w-xl lg:card-side bg-base-200 shadow-xl glass m-5">
-                <figure>
-                    <img
-                        src="https://www.hotel-icon.com/public/banner/images/nightviews%20banner.jpg"
-                        className="object-none w-96 lg:w-48 h-full pointer-events-none select-none"
-                        loading="lazy"
-                    />
-                </figure>
-                <div className="card-body">
-                    <h2 className="card-title justify-center">Login</h2>
-                    <div className="form-control w-full">
-                        <label className="label">
-                            <span className="label-text">Username</span>
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Username"
-                            id="username"
-                            className="input input-bordered w-full"
+        <>
+            <Toaster
+                position="bottom-center"
+                reverseOrder={false}
+                toastOptions={{ duration: 2000 }}
+            />
+            <div className="flex w-full h-screen items-center justify-center">
+                <div className="card max-w-xl lg:card-side bg-base-200 shadow-xl glass m-5">
+                    <figure>
+                        <img
+                            src="https://www.hotel-icon.com/public/banner/images/nightviews%20banner.jpg"
+                            className="object-none w-96 lg:w-48 h-full pointer-events-none select-none"
+                            loading="lazy"
                         />
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            id="password"
-                            className="input input-bordered w-full"
-                        />
-                    </div>
-                    <div className="card-actions mt-12">
-                        <button
-                            className="btn btn-outline w-full"
-                            onClick={verifyForm}
-                        >
-                            Login
-                        </button>
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title justify-center">Login</h2>
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text">Username</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Username"
+                                id="username"
+                                className="input input-bordered w-full"
+                            />
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                id="password"
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+                        <div className="card-actions mt-12">
+                            <button
+                                className="btn btn-outline w-full"
+                                onClick={verifyForm}
+                            >
+                                Login
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
@@ -63,6 +72,7 @@ function verifyForm() {
                 sessionStorage.setItem("role", data.role);
                 window.location.assign("/main");
             } else {
+                toast.error("Wrong username or password!");
             }
         });
 }
