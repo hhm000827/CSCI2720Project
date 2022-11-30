81554efbad401bde16ed7d802fea75c114fae5fa
Ho Him <hohim59@gmail.com>
2022-11-30 20:23:40 +0800

    delete google map search

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,14 +1,12 @@
-import { GoogleMap, MarkerF, StandaloneSearchBox, useJsApiLoader } from "@react-google-maps/api";
-import React, { useEffect, useState } from "react";
+import { GoogleMap, MarkerF, useJsApiLoader } from "@react-google-maps/api";
+import React, { useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import googleKey from "../config/googleKey.json";
 import "./GoogleMap.css";
-const library = ["places"];
 
 function ArtgoogleMap() {
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: googleKey.googleKey,
-    libraries: library,
   });
 
   if (loadError) {
@@ -18,30 +16,14 @@ function ArtgoogleMap() {
 }
 
 const Map = () => {
-  const [searchBox, setSearchBox] = useState(null);
   const [center, setCenter] = useState({
     lat: 22.38,
     lng: 114.177216,
   });
 
-  useEffect(() => {}, []);
-
-  const onLoad = (ref) => setSearchBox(ref);
-  const onPlacesChanged = () => {
-    let searchResult = () => {};
-    searchResult = searchBox.getPlaces();
-    setCenter({
-      lat: searchResult[0].geometry.location.lat(),
-      lng: searchResult[0].geometry.location.lng(),
-    });
-  };
-
   return (
     <GoogleMap id="googleMap" center={center} zoom={11}>
       <MarkerF position={center} />
-      <StandaloneSearchBox onLoad={onLoad} onPlacesChanged={onPlacesChanged}>
-        <input type="text" id="googleSearch" placeholder="Type your Location" />
-      </StandaloneSearchBox>
     </GoogleMap>
   );
 };
