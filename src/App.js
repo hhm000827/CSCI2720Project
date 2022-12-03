304b0d9b8b52bde4a6152e19bf146875e813a966
hhm000827 <hauhoman@yahoo.com.hk>
2022-12-03 22:54:35 +0800

    Fix: toast issue

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
+import { Toaster } from "react-hot-toast";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import { ShowEvent } from "./page/ShowEvent";
-import { ShowUserData } from "./page/ShowUserData";
 import { Location } from "./page/Location";
 import { Login } from "./page/Login";
 import Main from "./page/Main";
+import { ShowEvent } from "./page/ShowEvent";
+import { ShowUserData } from "./page/ShowUserData";
+
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="w-px font-normal hover:font-bold">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header> */}
       <div>
+        <Toaster position="top-center" reverseOrder={false} toastOptions={{ duration: 2000 }} />
         <BrowserRouter>
           <Routes>
             <Route path="/main" element={<Main />}></Route>
-            <Route path="/" element={<Login />}></Route>
             <Route path="/location/:venueName" element={<Location />}></Route>
             <Route path="/showEvent" element={<ShowEvent />}></Route>
             <Route path="/showUserData" element={<ShowUserData />}></Route>
+            <Route path="/*" element={<Login />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
