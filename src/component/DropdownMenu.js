308db0470413a9b3469b832cf7bd63bc352fbb54
hhm000827 <hauhoman@yahoo.com.hk>
2022-12-03 22:39:29 +0800

    add trash button

--- src/component/DropdownMenu.js
+++ src/component/DropdownMenu.js
@@ -34,6 +34,24 @@ const FavoriteLocationDropdown = (props) => {
       });
   };
 
+  const removeFavoriteList = (userName, venueName) => {
+    fetch("/deleteFavLoc", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: userName,
+        favLoc: venueName,
+      }),
+    })
+      .then((res) => (res.status === 200 ? res.json() : res.text()))
+      .then((data) => {
+        toast.success("Removed from favorites");
+        setFavoriteList((favoriteList) => favoriteList.pull(data));
+      });
+  };
+
   useEffect(() => {
     fetchFavoriteList(props.userName);
   }, [favoriteList]);
@@ -50,13 +68,24 @@ const FavoriteLocationDropdown = (props) => {
           {typeof favoriteList !== "undefined" && favoriteList.length > 0 ? (
             favoriteList.map((item) => {
               return (
-                <li>
-                  <p>
-                    {item}
-                    <button>1</button>
-                    <button>2</button>
-                  </p>
-                </li>
+                <div className="flex flex-row justify-between">
+                  <div>
+                    <li>
+                      <a href={`/location/${item.replace(/ /g, "_")}`}>{item}</a>
+                    </li>
+                  </div>
+                  <div>
+                    <button className="w-full h-full" onClick={(e) => removeFavoriteList(props.userName, item)}>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
               );
             })
           ) : (
