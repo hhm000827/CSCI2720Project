92d1a3b5eb903bcb0698ff998c2d6343dcdc775e
hhm000827 <hauhoman@yahoo.com.hk>
2022-12-03 22:01:38 +0800

    add favoriteList dropdown

--- src/component/DropdownMenu.js
+++ src/component/DropdownMenu.js
@@ -1,24 +1,73 @@
+import { useEffect, useState } from "react";
+
 const DropdownMenu = (props) => {
-    return (
-        <div className="dropdown dropdown-bottom mx-3">
-            <label tabIndex={0} className="btn btn-xs">
-                Sort
-            </label>
-            <ul
-                tabIndex={0}
-                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-                <li>
-                    <a onClick={() => props.sortingFunction(true)}>Sort asc</a>
-                </li>
-                <li>
-                    <a onClick={() => props.sortingFunction(false)}>
-                        Sort desc
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="dropdown dropdown-bottom mx-3">
+      <label tabIndex={0} className="btn btn-xs">
+        Sort
+      </label>
+      <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a onClick={() => props.sortingFunction(true)}>Sort asc</a>
+        </li>
+        <li>
+          <a onClick={() => props.sortingFunction(false)}>Sort desc</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
-export { DropdownMenu };
+const FavoriteLocationDropdown = (props) => {
+  const [favoriteList, setFavoriteList] = useState();
+
+  const fetchFavoriteList = (userName) => {
+    fetch(`/getFavLoc?username=${userName}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => (res.status === 200 ? res.json() : res.text()))
+      .then((data) => {
+        if (data.favoritelist) setFavoriteList(data.favoritelist);
+      });
+  };
+
+  useEffect(() => {
+    fetchFavoriteList(props.userName);
+  }, [favoriteList]);
+
+  return (
+    <ul className="menu menu-horizontal p-0">
+      <li tabIndex={0}>
+        <a>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="red">
+            <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+          </svg>
+        </a>
+        <ul className="rounded-box bg-base-100 p-2 z-10">
+          {typeof favoriteList !== "undefined" && favoriteList.length > 0 ? (
+            favoriteList.map((item) => {
+              return (
+                <li>
+                  <p>
+                    {item}
+                    <button>1</button>
+                    <button>2</button>
+                  </p>
+                </li>
+              );
+            })
+          ) : (
+            <li>
+              <p>Add Location here!</p>
+            </li>
+          )}
+        </ul>
+      </li>
+    </ul>
+  );
+};
+
+export { DropdownMenu, FavoriteLocationDropdown };
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { FavoriteLocationDropdown } from "./DropdownMenu";
 
 const isContained = (arr, str) => {
-  if (!arr) return false;
+  if (typeof arr === "undefined" || arr.length == 0) return false;
   return arr.some((element) => element.toLowerCase() === str.toLowerCase());
 };
 
@@ -24,8 +25,7 @@ const Nav = () => {
     })
       .then((res) => (res.status === 200 ? res.json() : res.text()))
       .then((data) => {
-        for (let i = 0; i < data.length; i++)
-          location.push(data[i]["venuename"]);
+        for (let i = 0; i < data.length; i++) location.push(data[i]["venuename"]);
         for (let i = 0; i < location.length; i++) {
           location[i] = location[i].split("(")[0];
           location[i] = location[i].slice(0, -1);
@@ -39,14 +39,10 @@ const Nav = () => {
   function handleKeyChange(event) {
     if (event.key === "Enter") {
       let keyword = document.querySelector("#searchBarInput").value;
-      if (!keyword || /^\s*$/.test(keyword))
-        toast.error("Empty typing is invalid");
-      else if (!isContained(locationList, keyword))
-        toast.error("Invalid location");
+      if (!keyword || /^\s*$/.test(keyword)) toast.error("Empty typing is invalid");
+      else if (!isContained(locationList, keyword)) toast.error("Invalid location");
       else {
-        let path = locationList.find(
-          (element) => element.toLowerCase() === keyword.toLowerCase()
-        );
+        let path = locationList.find((element) => element.toLowerCase() === keyword.toLowerCase());
         path = path.replace(/ /g, "_");
         window.location.assign(`/location/${path}`);
       }
@@ -55,13 +51,12 @@ const Nav = () => {
 
   function handleSearchKeywordChange() {
     let keyword = document.querySelector("#searchBarInput").value;
-    if (locationList) {
-      let result = locationList.filter((item) =>
-        item.toLowerCase().includes(keyword.toLowerCase())
-      );
-      if (result) setFilterList(result);
+    if (typeof locationList !== "undefined" && locationList.length > 0) {
+      let result = locationList.filter((item) => item.toLowerCase().includes(keyword.toLowerCase()));
+      if (typeof result !== "undefined" && result.length > 0) setFilterList(result);
     }
   }
+
   useEffect(() => {
     fetchLocationList();
   }, []);
@@ -70,13 +65,7 @@ const Nav = () => {
       <li tabIndex={0}>
         <a>
           Admin Panel
-          <svg
-            className="fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-          >
+          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
             <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
           </svg>
         </a>
@@ -91,13 +80,10 @@ const Nav = () => {
       </li>
     );
   }
+
   return (
     <>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{ duration: 2000 }}
-      />
+      <Toaster position="top-center" reverseOrder={false} toastOptions={{ duration: 2000 }} />
       <div className="navbar bg-base-300">
         <div className="flex-1">
           <span>
@@ -105,38 +91,19 @@ const Nav = () => {
           </span>
         </div>
         <div className="flex-none">
+          <FavoriteLocationDropdown userName={user} />
           <div className="form-control">
-            <input
-              id="searchBarInput"
-              list="locList"
-              type="text"
-              onKeyDown={(e) => handleKeyChange(e)}
-              onInput={handleSearchKeywordChange}
-              placeholder="Search"
-              className="input input-bordered"
-            />
+            <input id="searchBarInput" list="locList" type="text" onKeyDown={(e) => handleKeyChange(e)} onInput={handleSearchKeywordChange} placeholder="Search" className="input input-bordered" />
             <datalist id="locList">
               {filterList &&
                 filterList.map((item) => {
-                  if (
-                    item.toLowerCase() !==
-                    document
-                      .querySelector("#searchBarInput")
-                      .value.toLowerCase()
-                  )
-                    return <option>{item}</option>;
+                  if (item.toLowerCase() !== document.querySelector("#searchBarInput").value.toLowerCase()) return <option>{item}</option>;
                 })}
             </datalist>
           </div>
           <ul className="menu menu-horizontal p-0">
             <button className="btn btn-ghost btn-circle" onClick={backMain}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -147,22 +114,12 @@ const Nav = () => {
             </button>
             {adminBar}
             <button className="btn btn-ghost btn-circle" onClick={Logout}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-box-arrow-right"
-                viewBox="0 0 16 16"
-              >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
                 <path
                   fill-rule="evenodd"
                   d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
                 />
-                <path
-                  fill-rule="evenodd"
-                  d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-                />
+                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
               </svg>
             </button>
           </ul>
